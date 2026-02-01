a2eb618173c5225dc9534c04d671d3131e2d0c14
Daksh Jain <dakshjain@Dakshs-MacBook-Air.local>
2026-02-01 17:12:18 +0530

    allowed cors

--- Backend/index.js
+++ Backend/index.js
@@ -4,13 +4,7 @@ const cors = require("cors");
 
 // CORS configuration - allow frontend origin
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "http://localhost:5174",
-    process.env.FRONTEND_URL
-  ].filter(Boolean),
-  credentials: true,
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
